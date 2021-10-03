1ba92fe0280f91a16ad407d502c3389a33d0b62c
dymafr <erwan1309@hotmail.com>
2021-10-03 18:57:02 +0200

    1 file added

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TodosComponent } from './features/todos.component';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+];
